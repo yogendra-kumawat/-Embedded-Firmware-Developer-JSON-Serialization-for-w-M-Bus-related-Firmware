80896dfe63f416c908a259c1a985d18601eb05c8
yogendra-kumawat <yogendrakumawat2024@gmail.com>
2026-01-08 07:47:23 +0530

    Update json.c

--- include/json.c
+++ include/json.c
@@ -2,30 +2,28 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
  char* searlizer(input input_data ,int buff_length){
     meta m=input_data.m;
     readings r=input_data.r;
     data d=input_data.d;
     int dc=input_data.device_count;
    
-int leng=strlen(m.date)+strlen(m.deviceType)+strlen(m.gatewayId)+20;//as 10 is the max value count for 2^16(int_16 length)
+int leng=strlen(m.date)+strlen(m.deviceType)+strlen(m.gatewayId)+20;//(int)log10(m.interval_minutes)+(int)log10(m.total_readings);
 leng=leng+strlen(r.deviceId)+strlen(r.media)+strlen(r.meter)+strlen(r.unit);
-leng=leng+strlen(d.meter_datatime)+strlen(d.status)+strlen(d.timestamps)+10;
-leng=leng+285;//charactors in blank format for jaison;
-if (strlen(m.date)!=10){//format handling
+leng=leng+strlen(d.meter_datatime)+strlen(d.status)+strlen(d.timestamps)+10;//(int)log10(d.total_m3);
+leng=leng+385;//charactors in blank format;
+if (strlen(m.date)!=10){
     char* message="wrong data length for date (yyyy-mm-dd,10 characters)";
     return message;
 }
 
-if(buff_length<leng){//buffer size handling
+if(buff_length<leng){
     char * message="low buffer length";
     return message;
     
 }
 
-char *jaon =(char*)malloc(leng*sizeof(char));//memory assigning
-snprintf(jaon,leng,"[{\"gatewayId\": \"%s\",\"date\": \"%s\",\"deviceType\": \"%s\",\"interval_minutes\": %d,\"total_readings\": %d,\"values\": {\"device_count\": %d,\"readings\": [{\"media\": \"%s\", \"meter\": \"%s\",\"deviceId\": \"%s\",\"unit\": \"%s\",\"data\": [{\"timestamp\": \"%s\",\"meter_datetime\": \"%s\",\"total_m3\": %f,\"status\": \"%s\"}]}]}}]",m.gatewayId,m.date,m.deviceType,m.interval_minutes,m.total_readings,dc,r.media,r.meter,r.deviceId,r.unit,d.timestamps,d.meter_datatime,d.total_m3,d.status);
-
+char *jaon =(char*)malloc(leng*sizeof(char));
+snprintf(jaon,leng,"[\n\t{\n\t\t\"gatewayId\": \"%s\",\n\t\t\"date\": \"%s\",\n\t\t\"deviceType\": \"%s\",\n\t\t\"interval_minutes\": %d,\n\t\t\"total_readings\": %d,\n\t\t\"values\": {\n\t\t\t\"device_count\": %d,\n\t\t\t\"readings\": [\n\t\t\t{\"media\": \"%s\"\n\t\t\t\t, \"meter\": \"%s\",\n\t\t\t\t\"deviceId\": \"%s\",\n\t\t\t\t\"unit\": \"%s\",\n\t\t\t\t\"data\": [\n\t\t\t\t{\n\t\t\t\t\t\"timestamp\": \"%s\",\n\t\t\t\t\t\"meter_datetime\": \"%s\",\n\t\t\t\t\t\"total_m3\": %f,\n\t\t\t\t\t\"status\": \"%s\"\n\t\t\t\t\t\t}\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t}\n]\n",m.gatewayId,m.date,m.deviceType,m.interval_minutes,m.total_readings,dc,r.media,r.meter,r.deviceId,r.unit,d.timestamps,d.meter_datatime,d.total_m3,d.status);
 return jaon;
 }
